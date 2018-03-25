01add6269bb153d4b8b3bb7604c85d3379031231
vkoutha <vkoutha@gmail.com>
2018-03-25 19:20:07 -0400

    Update armControl.java

--- armControl.java
+++ armControl.java
@@ -33,27 +33,37 @@ public class armControl {
 	//Code written by Vinay
 	public void armStart(){
 		
-		MControl1.set(ControlMode.Velocity, GControl.getY()*-10);
+		if(GControl.getY()>-.05&& GControl.getY()<.05){
+		MControl1.set(ControlMode.Velocity, 0);	
+		}else{
+			
+			
+		MControl1.set(ControlMode.Velocity, GControl.getY()*13);
 		if(LSwitch1.get()==false){
 			//Switch 1 is Pressed
+			
+			MControl1.setIntegralAccumulator(0, 0, 0);
+			
 			if(GControl.getY()>0){
-				MControl1.set(ControlMode.Velocity, GControl.getY()*-10);
+				MControl1.set(ControlMode.Velocity, GControl.getY()*13);
 				//Switch 1 is Pressed, but it doesn't matter because the controller is going in the other direction
 			}else if(GControl.getY()<=0){
 				MControl1.set(ControlMode.Velocity, 0);
 				//Switch 1 is pressed, so the motor speed is set to 0
 			}
 		}else if(LSwitch2.get()==false){
+			
 			//Switch 2 is pressed
+			
 			if(GControl.getY()>=0){
 				MControl1.set(ControlMode.Velocity, 0);
 				//Switch 2 is pressed, so the motor speed is set to 0
 			}else if(GControl.getY()<0){
-				MControl1.set(ControlMode.Velocity, GControl.getY()*-10);
+				MControl1.set(ControlMode.Velocity, GControl.getY()*13);
 				//Switch 2 is pressed, but it doesn't matter because the controller is going in the other direction
 			}
 		}else if(LSwitch1.get()==true && LSwitch2.get()==true){
-			MControl1.set(ControlMode.Velocity, GControl.getY()*-10);
+			MControl1.set(ControlMode.Velocity, GControl.getY()*13);
 			//Switch 1 and Switch 2 are not pressed
 		}else if(LSwitch1.get()==false && LSwitch2.get()==false){
 			//Both Switch 1 and switch 2 are pressed
@@ -61,7 +71,9 @@ public class armControl {
 			MControl1.set(ControlMode.Velocity, 0);
 		}else{
 			//This is if switch 1 or switch 2 is not pressed
-			MControl1.set(ControlMode.Velocity, GControl.getY()*-10);
+			MControl1.set(ControlMode.Velocity, GControl.getY()*13);
+		}
+		
 		}
 	}
 	
