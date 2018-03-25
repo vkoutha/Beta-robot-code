a0543ff39b4587fecd2f384b7465fe69b139ee08
vkoutha <vkoutha@gmail.com>
2018-03-25 19:15:11 -0400

    Update encPosB.java

--- encPosB.java
+++ encPosB.java
@@ -4,13 +4,16 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 
 public class encPosB {
 
-	String gameData;
+	String gameData = "LRL";
 	
+	double AUTONSPEEDN;
+	double NAUTONSPEEDN;
 	//leftTalon and rightTalon
 	TalonSRX lTalN;
 	TalonSRX rTalN;
@@ -31,11 +34,16 @@ public class encPosB {
 	
 	Timer timerN;
 	
-	boolean intakeStop = false;
+	DoubleSolenoid lPiston;
+	DoubleSolenoid rPiston;
+
+	double CPI = 4096/18.84;
 	
-	//Code written by Vedh
+	boolean turn = false;
+	boolean timerStart = false;
+
 	public encPosB(TalonSRX rTal, TalonSRX lTal, VictorSPX rVic1, 
-			VictorSPX rVic2, VictorSPX lVic1, VictorSPX lVic2, Timer timer, TalonSRX armTal, VictorSPX armVic, TalonSRX intakeTal, VictorSPX intakeVic){
+			VictorSPX rVic2, VictorSPX lVic1, VictorSPX lVic2, Timer timer, TalonSRX armTal, VictorSPX armVic, TalonSRX intakeTal, VictorSPX intakeVic, DoubleSolenoid lPiston, DoubleSolenoid rPiston){
 		
 		lTalN = lTal;
 		rTalN = rTal;
@@ -48,6 +56,8 @@ public class encPosB {
 		armVicN = armVic;
 		intakeTalN = intakeTal;
 		intakeVicN = intakeVic;
+		this.lPiston = lPiston;
+		this.rPiston = rPiston;
 		
 	
 	}
@@ -57,202 +67,145 @@ public class encPosB {
 		//If we own the left side of the switch
 		if (gameData.charAt(0) == 'L'){
 			
-			//7610 total counts to get to the front of the switch (not including turns)
-			
-			//Monitor right talon for ease of turning
-			switch (rTalN.getSelectedSensorPosition(0)){
-			
-			//Go forward for 3000 counts
-			case 0:
+			//Go straight for 60 inches
+			if (rTalN.getSelectedSensorPosition(0) < (CPI * 40)){
 				
-				//Go straight
-				lTalN.set(ControlMode.PercentOutput, .5);
-				rTalN.set(ControlMode.PercentOutput, -.5);
-				
-				break;
-				
-			//Turn for 800 counts
-			case 3000: 
-				
-				//Turn left
-				lTalN.set(ControlMode.PercentOutput, -.3);
-				rTalN.set(ControlMode.PercentOutput, -.3);
-				
-				break;
-			
-			//Left encoder counts go to 2200 and right encoder counts go to 3800
-				
-			//Go forward for 5100 counts
-			case 3800:
-				
-				//Go straight (facing left)
-				lTalN.set(ControlMode.PercentOutput, .5);	
-				rTalN.set(ControlMode.PercentOutput, -.5);
-				
-				break;
-			
-			//Left encoder counts at 8100
-				
-			//Turn for 800 counts (encoder counts will reverse as well)
-			case 8900:
-				
-				//Turn right to the switch
-				lTalN.set(ControlMode.PercentOutput, .3);
-				rTalN.set(ControlMode.PercentOutput, .3);
-				
-				break;
-			
-			//Go forward for 4610
-			case 8100:
-				
-				//Making sure that it will only go forward after it turns
-				if(lTalN.getSelectedSensorPosition(0) >= 8100){
-				//Stop to raise the arm
 				lTalN.set(ControlMode.PercentOutput, .3);
 				rTalN.set(ControlMode.PercentOutput, -.3);
-				armTalN.set(ControlMode.Velocity, -6.5);
 				
-				}
+			//Turn left for 18.64 inches
+			}else if (rTalN.getSelectedSensorPosition(0) >= (CPI * 40) && rTalN.getSelectedSensorPosition(0) < (CPI * 63)){
 				
-				break;
+				lTalN.set(ControlMode.PercentOutput, -.35);
+				rTalN.set(ControlMode.PercentOutput, -.35);
 			
-			//Stops arm from moving
-			case 10000: 
+			//Go forward for about 81.5 inches
+			}else if (rTalN.getSelectedSensorPosition(0) >= (CPI * 63) && rTalN.getSelectedSensorPosition(0) < (CPI * 130) && turn == false){
 				
-				//Stop the arm at a certain point while still moving
-				armTalN.set(ControlMode.Velocity, -3);
+				lTalN.set(ControlMode.PercentOutput, .3);
+				rTalN.set(ControlMode.PercentOutput, -.3);
 				
-				break;
+			//Begin to turn right and raise the arm
+			}else if (lTalN.getSelectedSensorPosition(0) < (CPI * 113)){
 				
-			case 12710: 
+				turn = true;
 				
-				//Stop once at switch and spit out the cube
+				lTalN.set(ControlMode.PercentOutput, .35);
+				rTalN.set(ControlMode.PercentOutput, .35);
+				armTalN.set(ControlMode.Velocity, -10);
+
+				
+			//Go forward for about 79.5 inches
+			}else if (lTalN.getSelectedSensorPosition(0) < (CPI * 175)){
+				
+				lTalN.set(ControlMode.PercentOutput, .3);
+				rTalN.set(ControlMode.PercentOutput, -.3);
+				armTalN.set(ControlMode.Velocity, -2);
+				//lPiston.set(DoubleSolenoid.Value.kForward);
+				//rPiston.set(DoubleSolenoid.Value.kForward);
+				
+				
+			}else if (lTalN.getSelectedSensorPosition(0) >= (CPI *175) && timerN.get()<1.5){
+				
 				lTalN.set(ControlMode.PercentOutput, 0);
 				rTalN.set(ControlMode.PercentOutput, 0);
-				intakeTalN.set(ControlMode.PercentOutput, -.5);
+				intakeTalN.set(ControlMode.PercentOutput, -.25);
 				
-				break;
+				if(timerStart == false){
+				timerN.start();
+				}
 				
+				timerStart = true;
+
+			}else if (lTalN.getSelectedSensorPosition(0) >= (CPI*175) && timerN.get()>1.5){
 				
-				default: 
-					
-					
-					break;
-			
-			
-			}//End of switch statement
-			
-			
-			//Starting timer for intake to run
-			if (intakeTalN.getMotorOutputPercent() == -.5 && intakeStop == false){
-				timerN.reset();
-				intakeStop = true;
-			}
-			
-			//Stopping the intake 
-			if (timerN.get() > 1.5 && intakeStop == true){
 				intakeTalN.set(ControlMode.PercentOutput, 0);
+				armTalN.setIntegralAccumulator(0, 0, 0);
+				
+				
 			}
+			
+			if(DriverStation.getInstance().getMatchTime()<7){
+				
+				intakeTalN.set(ControlMode.PercentOutput, -.25);
+				
+			}
+			
+			
+			
 			
 //	  -------------------------------------------------------------			
 			
 		//If we own the right side of the switch
 		}else if (gameData.charAt(0) == 'R'){
 			
-			
-			switch (lTalN.getSelectedSensorPosition(0)){
-			
-			//Go forward for 3000 counts
-			case 0:
+			//Go forward for 60 inches
+			if (lTalN.getSelectedSensorPosition(0) < (CPI * 50)){
 				
-				//Go straight for a little bit
-				lTalN.set(ControlMode.PercentOutput, .5);
-				rTalN.set(ControlMode.PercentOutput, -.5);
-				
-				break;
-			
-			//Turn for 800 counts
-			case 3000:
-				
-				//Turn to the right
-				lTalN.set(ControlMode.PercentOutput, .5);
-				rTalN.set(ControlMode.PercentOutput, .5);
-				
-				break;
-			
-			//Right encoder goes to 2200 counts
-
-			//Go forward for 1200 counts
-			case 3800:
-				
-				//Go straight (facing the right)
-				lTalN.set(ControlMode.PercentOutput, .5);
-				rTalN.set(ControlMode.PercentOutput, -.5);
-				
-				break;
-				
-			//Right encoder goes to 3400 counts	
-			
-			//Turn for 800 counts (encoder counts will reverse due to turning direction)
-			case 5000:
-				
-				//Turn left to face the switch
-				lTalN.set(ControlMode.PercentOutput, -.3);
-				rTalN.set(ControlMode.PercentOutput, -.3);
-				
-				break;
-			
-			//Right encoder goes to 4200 counts
-				
-			//Go forward for 4610 counts
-			case 4200:
-				
-				if(rTalN.getSelectedSensorPosition(0)==4200){
-				//Begin raising arm and going towards the switch
 				lTalN.set(ControlMode.PercentOutput, .3);
 				rTalN.set(ControlMode.PercentOutput, -.3);
-				armTalN.set(ControlMode.Velocity, -6.5);
-				}
 				
-				break; 
+			//Turn for 18.5 inches (to the right)
+			}else if (lTalN.getSelectedSensorPosition(0) >= (CPI * 50) && lTalN.getSelectedSensorPosition(0) < (CPI * 73)){
+				
+				lTalN.set(ControlMode.PercentOutput, .35);
+				rTalN.set(ControlMode.PercentOutput, .35);
+				
+			//Go forward for 35.5 inches
+			}else if (lTalN.getSelectedSensorPosition(0) >= (CPI * 73) && lTalN.getSelectedSensorPosition(0) < (CPI * 109) && turn == false){
+				
+				lTalN.set(ControlMode.PercentOutput, .3);
+				rTalN.set(ControlMode.PercentOutput, -.3);
+				
+			//Turn for 18.5 inches (to the left)
+			}else if (rTalN.getSelectedSensorPosition(0) < (CPI * 91)){
+				
+				turn = true;
+				
+				lTalN.set(ControlMode.PercentOutput, -.3);
+				rTalN.set(ControlMode.PercentOutput, -.3);
+				armTalN.set(ControlMode.Velocity, -10);
+				//lPiston.set(DoubleSolenoid.Value.kForward);
+				//rPiston.set(DoubleSolenoid.Value.kForward);
+				
+				
+			//Go forward for about 81.5 inches
+			}else if (rTalN.getSelectedSensorPosition(0) < (CPI * 160)){
+				
+				turn = false;
+				lTalN.set(ControlMode.PercentOutput, .3);
+				rTalN.set(ControlMode.PercentOutput, -.3);
+				armTalN.set(ControlMode.Velocity, -2);
 			
-			//Stop arm at certain point from moving
-			case 6500:
+			//Stop once at switch and start the intake
+			}else if (rTalN.getSelectedSensorPosition(0) >= (CPI * 160) && timerN.get() < 1.5){
 				
-				//Stop the arm from moving
-				armTalN.set(ControlMode.Velocity, -3);
-				
-				break;
-			
-			//Stop moving and spit cube out
-			case 8810:
-				
-				//Stop driving and start the intake
 				lTalN.set(ControlMode.PercentOutput, 0);
 				rTalN.set(ControlMode.PercentOutput, 0);
-				intakeTalN.set(ControlMode.PercentOutput, -.5);
+				intakeTalN.set(ControlMode.PercentOutput, -.25);
 				
-				break;
+				if (timerStart == false){
+					timerN.start();
+				}
 				
-				default:
-					
-					
-					break;
-			
-			}//End of switch statement
-			
-			//Starting timer for intake to run
-			if (intakeTalN.getMotorOutputPercent() == -.5 && intakeStop == false){
-				timerN.reset();
-				intakeStop = true;
-			}
-			
-			//Stopping the intake
-			if (timerN.get() > 1.5 && intakeStop == true){
+				timerStart = true;
+				
+			}else if (rTalN.getSelectedSensorPosition(0) >= (CPI * 160) && timerN.get()> 1.5){
+				
 				intakeTalN.set(ControlMode.PercentOutput, 0);
+				armTalN.setIntegralAccumulator(0, 0, 0);
+				
 			}
 			
-		} //End of gameData if statement
+			if(DriverStation.getInstance().getMatchTime()<7){
+				
+				intakeTalN.set(ControlMode.PercentOutput, -.25);
+				
+			}
+			
+			
+			
+		}//End of gameData if statements
 		
 		
 		
